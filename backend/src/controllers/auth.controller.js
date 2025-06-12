14574a5cb05a0026bd884b0dd0a0677441cf88bf
aakashhh-17 <aakashbhujbal1704@gmail.com>
2025-06-12 11:12:18 +0530

    Created Stream user api route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -29,6 +30,17 @@ export const signUp = async (req, res) => {
       password,
       profilePic: randomAvatar,
     });
+
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || "",
+      });
+      console.log(`Stream user created for ${newUser.fullName}`)
+    } catch (error) {
+      console.log("Error creating stream user: ", error)
+    }
 
     const token = jwt.sign(
       { userId: newUser._id },
