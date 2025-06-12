7875dff1ca153fcd6cea396b9e8d6e8457e94755
aakashhh-17 <aakashbhujbal1704@gmail.com>
2025-06-12 22:02:55 +0530

    Created friend requests api routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -108,3 +108,36 @@ export const acceptFriendRequest = async (req, res) => {
     res.status(501).json({ message: "Internal server error" });
   }
 };
+
+export const getFriendRequests = async (req, res)=>{
+  try {
+    const incomingReqs = await FriendRequest.find({
+      recipient: req.user.id,
+      status: 'pending'
+    }).populate('sender', 'fullName profilePic nativeLanguage learningLanguage');
+
+    const acceptedReqs = await FriendRequest.find({
+      sender: req.user.id,
+      status: 'accepted'
+    }).populate('recipient', 'fullName profilePic');
+
+    res.status(200).json({ incomingReqs, acceptedReqs });
+  } catch (error) {
+    console.error("Error in getFriendRequests controller: ", error);
+    res.status(501).json({ message: "Internal server error" });
+  }
+}
+
+export const getOutgoingFriendReqs = async (req, res)=>{
+  try {
+    const outgoingReqs = await FriendRequest.find({
+      sender: req.user.id,
+      status: 'Pending'
+    }).populate('recipient', 'fullName profilePic')
+  
+     res.status(200).json(outgoingReqs);
+  } catch (error) {
+    console.error("Error in getOutgoingFriendReqs controller: ", error);
+    res.status(501).json({ message: "Internal server error" });
+  }
+}
